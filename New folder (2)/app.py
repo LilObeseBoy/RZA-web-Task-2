33452a67427394b9ee244570c2b557fbc37fe668
Max Wojciechowski <S2403570@ndonline.ac.uk>
2025-11-19 10:21:07 +0000

    version 1

--- New folder (2)/app.py
+++ New folder (2)/app.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def Home():  # put application's code here
+    return render_template("Home.html")
+@app.route('/Booking')
+def Booking():  # put application's code here
+    return render_template("Booking.html")
+@app.route('/ContactUs')
+def ContactUs():  # put application's code here
+    return render_template("ContactUs.html")
+@app.route('/LogIn')
+def LogIn():  # put application's code here
+    return render_template("LogIn.html")
+@app.route('/SignUp')
+def SignUp():  # put application's code here
+    return render_template("SignUp.html")
+@app.route('/Map')
+def Map():  # put application's code here
+    return render_template("Map.html")
+@app.route('/Animals')
+def Animals():  # put application's code here
+    return render_template("Animals.html")
 
+@app.route('/Experiences')
+def Experiences():  # put application's code here
+    return render_template("Experiences.html")
+
+@app.route('/Account')
+def Account():  # put application's code here
+    return render_template("Account.html")
 
 if __name__ == '__main__':
     app.run()
